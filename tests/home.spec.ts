ee9465ddc2e1b08b9203bc35e9b857f2d060bdb9
BugBuster <devrath.rana@bold.com>
2025-06-02 14:20:05 +0530

    css selector

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,12 +4,12 @@ test.describe("Home", () => {
   test("Open HomePage and verify title", async ({ page }) => {
     /*
     What happens if we skip await from below line.
-    
+
     Error: page.goto: Test ended.
     navigating to "https://practice.sdetunicorns.com/", waiting until "load"
     */
     //open url
-    await page.goto("https://practice.sdetunicorns.com/")
+    await page.goto("https://practice.sdetunicorns.com/");
 
     /*
     What happens when below assertion fails
@@ -21,6 +21,25 @@ test.describe("Home", () => {
     Received string: "Practice E-Commerce Site – SDET Unicorns"
     */
     //verify title
-    await expect(page).toHaveTitle("Practice E-Commerce Site – SDET Unicorns")
+    await expect(page).toHaveTitle("Practice E-Commerce Site – SDET Unicorns");
+  });
+  test("Open AboutPage and verify title", async ({ page }) => {
+    //open url
+    await page.goto("https://practice.sdetunicorns.com/about");
+
+    //verify title
+    await expect(page).toHaveTitle("About – Practice E-Commerce Site");
+  });
+  test("Click get started button using css selector", async ({ page }) => {
+    //open url
+    await page.goto("https://practice.sdetunicorns.com");
+
+    //  click the button
+    await page.locator("#get-started").click(); //id selector is used
+
+    //verify url has #get-started
+    await expect(page).toHaveURL(
+      "https://practice.sdetunicorns.com/#get-started"
+    );
   });
 });
